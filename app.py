0599eb4d8b1e79a0094ea3a55d2b5d051afb27ac
Dianyi Yang <dianyi.yang@politics.ox.ac.uk>
2026-03-17 20:00:24 +0000

    Scaffolding

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 from shiny import App, render, ui
 
-app_ui = ui.page_fluid()
+app_ui = ui.page_fluid(
+    ui.navset_tab(
+        ui.nav_panel("Carbon intensity", "Panel A content"),
+        ui.nav_panel("Energy intensity", "Panel B content"),
+        ui.nav_panel("Energy mix shares", "Panel C content"),
+        ui.nav_panel("Installed power generation capacity", "Panel D content"),
+        ui.nav_panel("Forest stock volume", "Panel E content"),
+        id="tab",
+    )
+)
+
+def server(input, output, session):
+    pass
+
+app = App(app_ui, server)
